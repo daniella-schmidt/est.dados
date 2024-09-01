fe63ba7c758d8a95360f2fd237b3f13161c4027c
daniella-schmidt <daniella.schmidt@unoesc.edu.br>
2024-09-01 16:48:44 -0300

    Update Exercicio_Vetor.cs

--- exercicios/Exercicio_Vetor.cs
+++ exercicios/Exercicio_Vetor.cs
@@ -1,29 +1,31 @@
+/* Escreva um algoritmo que armazene em dois vetores separados os primeiros 100 números pares e ímpares.
+No final, imprima a sequência armazenada em cada um deles. */
+
 using System;
 
 class Vetor{
     static void Main(){
         int[] pair = new int [100];
         int[] odd = new int [100];
-        
         int indexpair = 0;
         int indexodd = 0;
 
-        for (int i =1; i <= 200; i++){
+        for (int i = 1; i <= 200; i++){
             if (i % 2 == 0){
                 pair[indexpair] = i;
                 indexpair++;
             }
             else{
-                odd[indexodd]=1;
+                odd[indexodd] = i; 
                 indexodd++;
             }
         }
-        Console.WriteLine("pair numbers: ");
+        Console.WriteLine("Pair numbers: ");
         foreach (int number in pair)
         {
             Console.Write(number + " ");
         }
-        Console.WriteLine("\n\Odd Numbers: ")
+        Console.WriteLine("\nOdd Numbers: "); 
         foreach (int number in odd)
         {
             Console.Write(number + " ");
