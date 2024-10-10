1737b9fccbb52f457d96d555712c0e899f7fcdbb
Daniella Vitória Schmidt <433212@unoesc.lan>
2024-10-09 21:51:59 -0300

    aula de ordenação: Bubble & Insertion

---
+++ AlgoritmosOrdenacao/BubbleSort/Program.cs
@@ -0,0 +1,13 @@
+﻿using BubbleSort;
+
+int [] inteiros = {0,6,45,18,115,13};
+BubbleSort.BubbleSort.Print(inteiros);
+BubbleSort.BubbleSort.Sort<int>(inteiros);
+BubbleSort.BubbleSort.Print(inteiros);
+
+Console.WriteLine("");
+Console.WriteLine("Testar Char");
+char[]chars=['l','r','c'];
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
---
+++ AlgoritmosOrdenacao/InsertionSort/Program.cs
@@ -0,0 +1,6 @@
+﻿using InsertionSort;
+
+int [] inteiros = {95,65,13,2,1,0,45,456};
+InsertionSort.InsertionSort.Print(inteiros);
+InsertionSort.InsertionSort.Sort<int>(inteiros);
+InsertionSort.InsertionSort.Print(inteiros);
