924f0cebcc017dec48d4a6bf195cada954c1c7e0
danocaboboca <tori.messy@gmail.com>
2024-10-21 19:45:46 -0300

    implementação

---
+++ exercicios/teste/Program.cs
@@ -0,0 +1,18 @@
+﻿int [,] num = new int [10,10];
+
+for (int i = 0; i < num.GetLength(0); i++)
+{
+ for (int j = 0; j < num.GetLength(1); j++)
+ {
+ num[i, j] = (i + 1) * (j + 1);
+ }
+}
+
+for (int i = 0; i < num.GetLength(0); i++)
+{
+ for (int j = 0; j < num.GetLength(1); j++)
+ {
+ Console.Write("{0,4}", num[i, j]);
+ }
+ Console.WriteLine();
+}
