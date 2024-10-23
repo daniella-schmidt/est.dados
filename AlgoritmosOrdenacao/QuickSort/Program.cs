d3356864b45d7dc4e16559058793500498c45bb2
Daniella Vitória Schmidt <433212@unoesc.lan>
2024-10-23 19:34:12 -0300

    agora vai

---
+++ AlgoritmosOrdenacao/QuickSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using QuickSort;
+
+Console.WriteLine("Testando o Quick Sort!");
+
+int [] values = new int []{96,65,2,3,5,2,68,51};
+
+QuickSort.QuickSort.Print<int>(values);
+Console.WriteLine("Iniciando a ordenação");
+QuickSort.QuickSort.Sort<int>(values, 0, values.Length - 1);
+Console.WriteLine("Ordenação finalizada");
+QuickSort.QuickSort.Print<int>(values);
---
+++ AlgoritmosOrdenacao/SelectionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using SelectionSort;
+
+Console.WriteLine("Numbers:");
+int[] values = { 0, 6, 45, 18, 115, 13 };
+SelectionSort.SelectionSort.Print(values);
+
+Console.WriteLine();
+
+Console.WriteLine("In order:");
+SelectionSort.SelectionSort.Sort<int>(values);
+SelectionSort.SelectionSort.Print(values);
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Names:");
+string[] names = { "Storm", "Nightcrawler", "Gambit", "Magneto" }; 
+SelectionSort.SelectionSort.Print(names);
+
+Console.WriteLine();
+
+Console.WriteLine("In order:");
+SelectionSort.SelectionSort.Sort(names); 
+SelectionSort.SelectionSort.Print(names);
